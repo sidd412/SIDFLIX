93a56e8ad66b0d218d778c086df0c2fb2986839c
siddhartha verma <siddharthaverma6213@gmail.com>
2023-03-13 17:39:48 +0530

    d

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import Main from './TodoCompo/Main'
 import Header from './components/Header'
 import Login from './components/Login'
 import Home from './components/Home'
+import CourseGoalItem from './components2/CourseGoals/CourseGoalItem/CourseGoalItem'
+import  CourseInput from './components2/CourseGoals/CourseInput/CourseInput'
+import CourseGoalList from './components2/CourseGoals/CourseGoalList/CourseGoalList'
+
 function App() {
   return (
    <>
-   <Main/>
+  
+   <CourseInput/>
+   <CourseGoalItem/>
+   {/* <CourseGoalList/> */}
    </>
   );
 }
--- src/TodoCompo/Main.js
+++ src/TodoCompo/Main.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+const tasks = JSON.parse(localStorage.getItem("todos")) || [];
 
-export default function main() {
+export default function Main() {
+  const [task, setTask] = useState("");
+
+  function addTask() {
+    tasks.push(task);
+    localStorage.setItem("todos", JSON.stringify(tasks));
+    alert("task is stored locally in todos array");
+  }
 
   return (
     <div>
-      <h1>To Do APP</h1>
-      <input type="text" onChange={fun} /><br/><br/>
-      <button>Add Product</button>
-      <h1>List of To DO Tasks:</h1>
+      <h1>To Do tasks Chart APP</h1>
+      <h3>
+        <label htmlFor="task">Mention new Task : </label>
+      </h3>
+      <input
+        type="text"
+        id="task"
+        value={task}
+        required
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <br />
+      <br />
+      <button type="submit" id="btn" onClick={addTask}>
+        Add TASK
+      </button>
     </div>
-  )
+  );
 }
