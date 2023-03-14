0ccd8547a69831bb95a96dab69c4ba591cdeea22
siddhartha verma <siddharthaverma6213@gmail.com>
2023-03-14 12:41:54 +0530

    dddds

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import './App.css';
-import Main from './TodoCompo/Main'
-import Header from './components/Header'
-import Login from './components/Login'
-import Home from './components/Home'
-import CourseGoalItem from './components2/CourseGoals/CourseGoalItem/CourseGoalItem'
-import  CourseInput from './components2/CourseGoals/CourseInput/CourseInput'
-import CourseGoalList from './components2/CourseGoals/CourseGoalList/CourseGoalList'
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-   <>
-  
-   <CourseInput/>
-   <CourseGoalItem/>
-   {/* <CourseGoalList/> */}
-   </>
-  );
+    <div>
+      
+    </div>
+  )
 }
 
-export default App; 
+
